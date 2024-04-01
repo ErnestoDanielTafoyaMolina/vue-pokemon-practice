ccb6ccaae3f715d8aad57c9f9d333b871e88bb37
ErnestoDanielTafoyaMolina <edaniimol@gmail.com>
2024-03-31 18:58:14 -0600

    first commit
    first configuration

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>¿Quien es este Pókemon?</h1>
+    <PokemonPicture />
+    <PokemonOptions />
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+export default {
+    components:{
+        PokemonPicture,
+        PokemonOptions
+    },
+}
+</script>
+
